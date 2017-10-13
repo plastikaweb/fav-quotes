e0c4878ed158622ebd320e58fd4695c0cc3f7e33
Carlos Matheu Armengol <info@plastikaweb.com>
2017-10-13 10:46:25 +0200

    refactor final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,19 +13,19 @@ import { SettingsPage } from '../pages/settings/settings';
 import { TabsPage } from '../pages/tabs/tabs';
 import { QuotesService } from '../services/quotes.service';
 import { SettingsService } from '../services/settings.service';
+import { FavoritesPageModule } from '../pages/favorites/favorites.module';
+import { LibraryPageModule } from '../pages/library/library.module';
+import { QuotesPageModule } from '../pages/quotes/quotes.module';
+import { QuotePageModule } from '../pages/quote/quote.module';
+import { SettingsPageModule } from '../pages/settings/settings.module';
 
 @NgModule({
   declarations: [
     MyApp,
-    FavoritesPage,
-    LibraryPage,
-    QuotesPage,
-    QuotePage,
-    SettingsPage,
     TabsPage
   ],
-  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
-  bootstrap: [IonicApp],
+  imports: [ BrowserModule, IonicModule.forRoot(MyApp), FavoritesPageModule, LibraryPageModule, QuotesPageModule, QuotePageModule, SettingsPageModule ],
+  bootstrap: [ IonicApp ],
   entryComponents: [
     MyApp,
     FavoritesPage,
